209c7a86aef6b8571b9df892a635af248f27d290
harshitchelanii <chelaniharshit007@gmail.com>
2026-07-26 18:25:02 +0000

    Keep server running for multiple client sessions

--- server.c
+++ server.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,55 +7,70 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-int main(void){
+int main(void)
+{
     printf("TCP server is starting...\n");
 
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1)
-    {
+    if (server_fd == -1) {
         perror("socket");
         return 1;
     }
-    printf("Socket Created successsfully\n");
+    printf("Socket Created successfully\n");
 
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(8080);
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-        {
-            perror("bind");
-            return 1;
+
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        return 1;
+    }
+
+    printf("Socket bound successfully\n");
+
+    if (listen(server_fd, 5) == -1) {
+        perror("listen");
+        return 1;
+    }
+
+    while (1) {
+        printf("Waiting for a client...\n");
+
+        int client_fd = accept(server_fd, NULL, NULL);
+        if (client_fd == -1) {
+            perror("accept");
+            continue;
         }
-    printf("socket bound sucessfully\n");
-    if(listen(server_fd, 5) == -1){
-    perror("Listen fail");
-    exit(EXIT_FAILURE);
-    }
-    printf("Waiting for a client...\n");
-    int client_fd = accept(server_fd,NULL, NULL);
-    if(client_fd == -1){
-        perror("accept failed");
-        exit(EXIT_FAILURE);
-    }
-    else{
-        printf("Client connection sucessfull!\n");
-    }
-    char buffer[1024];
-    while(1){
-    int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-    if(bytes_received == -1){
-        perror("recv failed");
-        exit(EXIT_FAILURE);
-    }
-    if(bytes_received == 0){
-        printf("Client disconnected.\n");
+
+        printf("Client connection successful!\n");
+
+        char buffer[1024];
+
+        while (1) {
+
+            int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+
+            if (bytes_received == -1) {
+                perror("recv");
+                break;
+            }
+
+            if (bytes_received == 0) {
+                printf("Client disconnected.\n");
+                break;
+            }
+
+            buffer[bytes_received] = '\0';
+            printf("Received: %s\n", buffer);
+        }
+
         close(client_fd);
     }
-    else{
-        buffer[bytes_received] = '\0';
-        printf("Received : %s\n", buffer);
-    }}
+
+    close(server_fd);
     return 0;
-    }
+}
